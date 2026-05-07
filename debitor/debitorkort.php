f53d257aadad7f821c36135da86d4e5a2ef7a830
Peter Rude <phr@danosoft.dk>
2026-05-07 19:46:44 +0300

    Addded felt_1 - Felt_5 to selection

--- debitor/debitorkort.php
+++ debitor/debitorkort.php
@@ -2470,9 +2470,9 @@ function split_navn($firmanavn)
 // echo substr($cvrnr,0,1)."<br>";
 
 
-if (!$id || substr($cvrnr,0,1)  == '*') {
-	$cvrnr = trim($cvrnr,"*");
-	echo substr($cvrnr,0,1)."<br>";
+if (!$id || substr($cvrnr, 0, 1)  == '*') {
+	$cvrnr = trim($cvrnr, "*");
+	echo substr($cvrnr, 0, 1)."<br>";
 	print "<script language=\"javascript\" type=\"text/javascript\" src=\"../javascript/cvrapiopslag.js\"></script>\n";
 }
 
