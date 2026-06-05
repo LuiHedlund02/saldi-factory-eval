b492629bc9f3dc30b7a05f9a0764a7319923c202
Saul Zayn <zaynsaul@gmail.com>
2026-06-05 08:27:57 +0000

    Fix: false unsaved popup when creating new creditor order

--- kreditor/ordre.php
+++ kreditor/ordre.php
@@ -1362,10 +1362,11 @@ function ordreside($id) {
 	}
 	print "</tbody></table></td></tr>\n";
 	print "</form>";
+	print "<script>docChange = false;</script>";
 	print "</tbody></table></td></tr></tbody></table></td></tr>\n";
 	print "<tr><td></td></tr>\n";
 
-		
+
 }# end function ordreside
 ######################################################################################################################################
 
